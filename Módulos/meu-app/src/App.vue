76c73b30f5a3f67f025edaff1dff868843359b99
Jonas Soares <78958207+Jonah21-prog@users.noreply.github.com>
2022-01-11 22:26:57 -0300

    ...

--- Módulos/meu-app/src/App.vue
+++ Módulos/meu-app/src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  
+  <div id="app">
+    <h1>Bem vindo ao meu projeto</h1>
+    <usuario/>
+  </div>
 </template>
 
 <script>
-
+import Usuario from "./components/Usuario.vue";
 
 export default {
-  name: 'App', 
-  
+  name: "App",
+  components: {
+    Usuario,
+  },
+  data(){
+    return{
+      nome: 'Sujeito programador'
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ Módulos/meu-app/src/components/Usuario.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="usuario">
+    <h2>Olá: sujeito programador</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "usuario",
+};
+</script>
+
+<style>
+</style>
