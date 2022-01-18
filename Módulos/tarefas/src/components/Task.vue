7dd014832a5a8520888b2c0472aead6ae63f28c8
[Jonah21-prog] <jonas.eng.software@gmail.com>
2022-01-18 11:25:28 -0300

    App tarefas deu erro, então recriei ele...

--- Módulos/tarefas/src/components/Task.vue
+++ Módulos/tarefas/src/components/Task.vue
@@ -1,21 +1,58 @@
 <template>
     <div id="task">
         
-       <form>
+       <form @submit.prevent="addItem" >
            <input
            type="text"
            placeholder="Tarefa de hoje?"
+           v-model="tarefa"
            />
            <button type="submit">Adicionar</button>
        </form>
+
+       <Item :lista="tarefas" :delete="deleteTask" />
 
     </div>
 </template>
 
 
 <script>
+import Item from './Item';
 export default {
-    name: 'Task'
+    name: 'Task',
+    components:{
+        Item
+    },
+    data(){
+        return{
+            tarefa: '',
+            tarefas: [],
+        }
+    },
+    methods:{
+        addItem(){
+           if(this.tarefa !== ''){
+               this.tarefas.push({
+                   text: this.tarefa,
+                   key: Date.now(),
+               });
+           }else{
+               alert('Digite uma tarefa..');
+               return;
+           }
+
+            this.tarefa = '';
+            console.log(this.tarefas);
+
+        },
+        deleteTask(key){
+           let filtro = this.tarefas.filter( (item) => {
+              return (item.key !== key);
+           });
+
+           return this.tarefas = filtro;
+        }
+    }
 }
 </script>
 
